7d666cc04bc2e02eb409371b4a30e2e599ac02a7
PatrickSunico <apatricksunico@gmail.com>
2021-10-10 21:55:09 +0300

    initialize backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bc13a6b9b91497466b92d1cce5597fa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '24a78b82-a0ee-4bc1-8ddf-403dd4484f86'
+};
