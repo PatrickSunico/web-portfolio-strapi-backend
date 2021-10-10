1ed4c2ff5e692235cce06a8c09cd1467d57421c5
PatrickSunico <apatricksunico@gmail.com>
2021-10-11 01:34:12 +0300

    Added some fields in the backend

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 8082),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '9bc13a6b9b91497466b92d1cce5597fa'),
+      secret: env("ADMIN_JWT_SECRET", "9bc13a6b9b91497466b92d1cce5597fa"),
     },
   },
 });
